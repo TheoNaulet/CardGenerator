d9a4f1686c752cac27fcdfd5af4618558adb6b17
Théo Naulet <theo.naulet@gmail.com>
2022-07-28 08:08:49 +0200

    CORS problem fixed

--- src/App.js
+++ src/App.js
@@ -4,34 +4,43 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [data, setData ] = useState('')
-  const url = `https://api.namefake.com/french-france`; 
+  const [data, setData ] = useState('');
+  const [nom, setNom] = useState('');
+  const url = `https://cors-anywhere.herokuapp.com/https://api.namefake.com/french-france`; 
   const getName = () => {
     axios.get(url, {
-      headers:{"Access-Control-Allow-Origin": "*"}
+      headers:{
+        'Content-Type': 'application/json;charset=UTF-8',
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',}
     }).then((response) => {
       setData(response.data)
-      console.log(response.data)
+      // setNom = response.data.name;
+      // console.log(nom);
     })
     .catch(error => console.log(error));
-  };
+  }; 
 
   useEffect(() => {
     getName()
   }, []);
 
+  function refreshPage(){
+    window.location.reload();
+  }
+
   return (
 
     <div id='App'>
       <div className='button'>
-        <button>Generate card</button>
+        <button onClick={refreshPage}>Generate card</button>
       </div>
       <div id='card'>
         <h2 id='bank'>Bank</h2>
         <div id='bottomcard'>
           <div id='left'>
             <h1 id='number'>{Math.floor(Math.random()*(999-100)+100)} {Math.floor(Math.random()*(999-100)+100)} {Math.floor(Math.random()*(999-100)+100)} {Math.floor(Math.random()*(999-100)+100)}</h1>
-            <h2 id='name'>Your Name</h2>
+            <h2 id='name'>{data.name}</h2>
           </div>
           <div id='right'>
             <img src='../Visa.svg.png' width={100}></img>
