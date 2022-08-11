824bf1a1b4fd4b74956ee893c62e539c75f3ed6d
Théo Naulet <theo.naulet@gmail.com>
2022-08-11 10:42:09 +0200

    Cors problem fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React,{ useEffect, useState } from 'react';
+
 
 function App() {
-
   const [data, setData ] = useState('');
-  const [nom, setNom] = useState('');
   const url = `https://cors-anywhere.herokuapp.com/https://api.namefake.com/french-france`; 
   const getName = () => {
     axios.get(url, {
       headers:{
         'Content-Type': 'application/json;charset=UTF-8',
-      'Access-Control-Allow-Origin' : '*',
-      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',}
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',}
     }).then((response) => {
-      setData(response.data)
-      // setNom = response.data.name;
-      // console.log(nom);
+        setData(response.data)
+        // setNom = response.data.name;
+        // console.log(nom);
     })
     .catch(error => console.log(error));
   }; 
@@ -29,6 +28,10 @@ function App() {
     window.location.reload();
   }
 
+  function randomNumber(){
+    return Math.floor(Math.random()*(999-100)+100)
+  }
+
   return (
 
     <div id='App'>
@@ -39,16 +42,17 @@ function App() {
         <h2 id='bank'>Bank</h2>
         <div id='bottomcard'>
           <div id='left'>
-            <h1 id='number'>{Math.floor(Math.random()*(999-100)+100)} {Math.floor(Math.random()*(999-100)+100)} {Math.floor(Math.random()*(999-100)+100)} {Math.floor(Math.random()*(999-100)+100)}</h1>
-            <h2 id='name'>{data.name}</h2>
+            <h1 id='number'>{randomNumber()} {randomNumber()} {randomNumber()} {randomNumber()}</h1>
+            <h2 id='name'>{data?.name}</h2>
           </div>
           <div id='right'>
             <img src='../Visa.svg.png' width={100}></img>
           </div>
       </div>
     </div>
-    <div id='ball1' className='ball'></div>
-      <div id='ball2' className='ball'></div>
+    <div id='ball1' className='ball' />
+    <div id='ball2' className='ball' />
+
   </div> 
   );
 }
